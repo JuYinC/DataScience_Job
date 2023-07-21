72f56bead032609838801e8a409948da6581c320
JuyinC <juyinchao@gmail.com>
2023-07-21 18:11:19 +0800

    Create scraper

---
+++ glassdoor_scraper.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import pandas as pd
+
+l=list()
+o={}
+
+target_url = "https://www.glassdoor.com/Job/data-science-jobs-SRCH_KO0,12.htm?clickSource=searchBox"
+
+options = webdriver.ChromeOptions()
+
+driver=webdriver.Chrome()
+
+driver.get(target_url)
+
+driver.maximize_window()
+time.sleep(2)
+
+resp = driver.page_source
+
+driver.close()
+
+soup=BeautifulSoup(resp,'html.parser')
+
+allJobsContainer = soup.find("ul",{"class":"css-7ry9k1"})
+allJobs = allJobsContainer.find_all("li")
+
+for job in allJobs:
+    try:
+        o["name-of-company"]=job.find("div",{"class":"d-flex align-items-center"}).text
+    except:
+        o["name-of-company"]=None
+    try:
+        o["name-of-job"]=job.find("div",{"class":"job-title mt-xsm"}).text
+    except:
+        o["name-of-job"]=None
+    try:
+        o["location"]=job.find("div",{"class":"location mt-xxsm"}).text
+    except:
+        o["location"]=None
+    try:
+        o["salary"]=job.find("div",{"class":"salary-estimate"}).text
+    except:
+        o["salary"]=None
+    l.append(o)
+    o={}
+
+print(l)
+
+df = pd.DataFrame(l)
+df.to_csv('jobs.csv', index=False, encoding='utf-8')
